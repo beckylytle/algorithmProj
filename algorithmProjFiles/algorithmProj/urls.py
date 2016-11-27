fc59fa45da0c7f42b68e1daa8971bef3a1f62ae3
Becky Lynn Lytle <beckylynn1@Rebeccas-MacBook.local>
2016-11-26 23:59:14 -0500

    trying to get this on server

--- algorithmProjFiles/algorithmProj/urls.py
+++ algorithmProjFiles/algorithmProj/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from algorithmProj.views import index,ShortestPathGame
+from alg import views
 
 urlpatterns = [
     # Examples:
@@ -8,9 +8,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', index, name='index'),
+    url(r'^$', views.index, name='index'),
     #url(r'^Main$', 'alg.views.ShortestPathMain', name = "shortestPath"),
-    url(r'^Game$', ShortestPathGame, name = "shortestPathGame"),
+    url(r'^Game$', views.ShortestPathGame, name = "shortestPathGame"),
     #url(r'^Result$', 'alg.views.ShortestPathResult', name = "shortestPathResult"),
     #url(r'^HowTo$', 'alg.views.dijkstra', name = "dijkstra"),
     #url(r'^graph.json$', 'alg.views.json', name = "json"),
